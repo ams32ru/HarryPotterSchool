d088ff171d8b9a4a2f00064a87da0841f0129bb4
ams32ru <ams32ru@mail.ru>
2022-12-22 21:24:58 +0300

    version 7

--- src/main/java/ru/hogwarts/school/scripts421.sql
+++ src/main/java/ru/hogwarts/school/scripts421.sql
@@ -2,7 +2,7 @@ ALTER TABLE student
     ADD CONSTRAINT age_constraint CHECK ( age > 16 );
 
 ALTER TABLE student
-    ADD PRIMARY KEY (name);
+    ADD UNIQUE  (name);
 
 ALTER TABLE student
     ALTER COLUMN age SET DEFAULT 20;
