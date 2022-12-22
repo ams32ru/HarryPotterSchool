44b6812698888ee46d89c7418fcc7b70320dce4f
ams32ru <ams32ru@mail.ru>
2022-12-22 22:17:24 +0300

    version 7.1

--- src/main/resources/liquibase/scripts/index-practice.sql
+++
@@ -1,7 +0,0 @@
---liquibase formatted sql
-
---changeset asobolev:1
-CREATE INDEX student_name_index on student (name);
-
---changeset asobolev:2
-CREATE INDEX faculty_name_color_index on faculty (name, color);
